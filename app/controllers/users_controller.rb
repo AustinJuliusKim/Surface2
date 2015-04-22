295845ddbe181c25a6a09a939dac774e9911b101
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-04-21 18:47:58 -0700

    Add before_action and before_save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :require_login, except: [:new, :create]
+	before_action :authorized?, only: [:edit, :update]
+
 	def index
 		@users = User.all
 	end
@@ -16,7 +19,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			[session[:user_id] = @user.id.to_s]
-			flash[:welcome] = "Thanks for signup up, #{@user.first_name}"
+			flash[:welcome] = "Thanks for signing up, #{@user.first_name}"
 			redirect_to users_path
 		else
 			render :new
@@ -43,6 +46,20 @@ class UsersController < ApplicationController
 		redirect_to users_path
 	end
 	private
+
+	def require_login
+		unless logged_in?
+			flash[:error] = "You must be logged in to access that page."
+			redirect_to login_path
+		end
+	end
+
+	def authorized?
+		unless current_user == User.find(params[:id])
+			flash[:error] = "You are not authorized to access that page."
+			redirect_to user_path
+		end
+	end
 
 	def user_params
 		params.require(:user).permit(:username, :password, :first_name, :last_name, :email, :age, :gender, :looking_for)
